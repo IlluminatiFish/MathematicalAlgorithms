a324453ff4e29cf53fed75a5af5fbfaa71f3d6b0
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2021-03-12 22:11:50 +0000

    Update LogicalBitwiseMathematics.py

--- Algorithms/Computing/LogicalBitwiseMathematics.py
+++ Algorithms/Computing/LogicalBitwiseMathematics.py
@@ -6,22 +6,17 @@ from math import log
 
 '''
     @function left_shift - Finds all the variables within a left bit shift operation
+    @param k - The value of the right side ("prefix") of the bitwise operation
+    @param n - The value of the left side of the bitwise operation
+    @param y - The output of the bitwise operation   
+    @const l - The constant found in both shift equations to find the @param n 
 
     Operation must have the form:
         k << n = y
 '''
 
 def left_shift(k=None, n=None, y=None):
-    
-    '''
-        @param k - The value of the right side ("prefix") of the bitwise operation
-        @param n - The value of the left side of the bitwise operation
-        @param y - The output of the bitwise operation           
-    '''
 
-    '''
-        @const l - The constant found in both shift equations to find the @param n 
-    '''
     l = log(2)
 
     # If k is not given find the value of k 
@@ -41,22 +36,17 @@ def left_shift(k=None, n=None, y=None):
 
 '''
     @function right_shift - Finds all the variables within a right bit shift operation
+    @param k - The value of the right side ("prefix") of the bitwise operation
+    @param n - The value of the left side of the bitwise operation
+    @param y - The output of the bitwise operation    
+    @const l - The constant found in both shift equations to find the @param n 
 
     Operation must have the form:
         k >> n = y
 '''
 
 def right_shift(k=None, n=None, y=None):
-    
-    '''
-        @param k - The value of the right side ("prefix") of the bitwise operation
-        @param n - The value of the left side of the bitwise operation
-        @param y - The output of the bitwise operation           
-    '''
 
-    '''
-        @const l - The constant found in both shift equations to find the @param n 
-    '''
     l = log(2)
 
     # If k is not given find the value of k 
